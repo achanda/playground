6fb72690adc61560f75ffa187c0518533355f611
Abhishek Chanda <abhishek@cloudscaling.com>
2015-03-29 00:29:08 -0700

    Tweaks

--- coreos_gce.sh
+++ coreos_gce.sh
@@ -7,7 +7,7 @@ set -x
 NUM_NODES=${NUM_NODES:-3}
 ETCD_DISCOVERY_URL=$(curl --silent https://discovery.etcd.io/new?size=$NUM_NODES)
 IMAGE=$(gcloud compute images list | grep 'coreos-stable*' | cut -d" " -f 1)
-METADATA="http://metadata/computeMetadata/v1/"
+METADATA="http://metadata/computeMetadata/v1"
 
 for N in `seq 1 $NUM_NODES`
 do
@@ -16,12 +16,13 @@ do
         --image $IMAGE \
         --zone "us-central1-a" \
         --machine-type "n1-standard-4" \
-        --metadata startup-script="#!/bin/sh
+        --metadata startup-script="#! /bin/sh
         ETCD_DISCOVERY_URL=$ETCD_DISCOVERY_URL
         HOSTNAME=\$(curl $METADATA/hostname)
         IPADDR=\$(ifconfig eth0| grep 'inet ' | cut -dt -f2 | awk '{ print \$1}')
         sudo systemctl start fleet
         etcd -name \$HOSTNAME -peer-addr \$IPADDR:7001 -addr \$IPADDR:4001 -discovery \$ETCD_DISCOVERY_URL
-        "
+        EOF"
+
         echo "Created instance core-$N"
 done
