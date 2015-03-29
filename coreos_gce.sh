10d927e4f6f46be316e177013d8bc7d175b0c0cc
Abhishek Chanda <abhishek@cloudscaling.com>
2015-03-29 13:26:53 -0700

    Use an image from coreos project

--- coreos_gce.sh
+++ coreos_gce.sh
@@ -6,7 +6,7 @@ set -x
 
 NUM_NODES=${NUM_NODES:-3}
 ETCD_DISCOVERY_URL=$(curl --silent https://discovery.etcd.io/new?size=$NUM_NODES)
-IMAGE=$(gcloud compute images list | grep 'coreos-stable*' | cut -d" " -f 1)
+IMAGE="https://www.googleapis.com/compute/v1/projects/coreos-cloud/global/images/coreos-stable-607-0-0-v20150317"
 METADATA="http://metadata/computeMetadata/v1"
 
 for N in `seq 1 $NUM_NODES`
@@ -14,9 +14,10 @@ do
         gcloud compute instances create core-$N \
         --project $1 \
         --image $IMAGE \
-        --zone "us-central1-a" \
-        --machine-type "n1-standard-4" \
-        --metadata startup-script="#! /bin/sh
+        --zone us-central1-a \
+        --machine-type n1-standard-4 \
+        --metadata startup-script=
+        "#! /bin/sh
         ETCD_DISCOVERY_URL=$ETCD_DISCOVERY_URL
         HOSTNAME=\$(curl $METADATA/hostname)
         IPADDR=\$(ifconfig eth0| grep 'inet ' | cut -dt -f2 | awk '{ print \$1}')
